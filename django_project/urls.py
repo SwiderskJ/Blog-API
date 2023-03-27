73984e44215686961b4f7dfdd2d21a7dbd909b5d
SwiderskJ <janswiderski@op.pl>
2023-03-27 14:06:09 +0200

    Schemas and Documentation

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 import posts.urls
 
@@ -24,5 +25,8 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema',),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(), name='redoc',),
+    path('api/schema/swagger/', SpectacularSwaggerView.as_view(), name='swagger',)
 
 ]
